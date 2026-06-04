d3f1a19596e1e9b7e25a87c3211e7318a193a511
Zirulan <ilinsergij4298@gmail.com>
2026-06-04 13:26:08 +0300

    add: header more wigets

---
+++ src/components/header/call.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="call">
+        <div class="phone_number">
+            <img src="../../assets/icons/PhoneCall.svg">
+            <h2>+90 212 547 08 26</h2>
+        </div>
+        <div class="second_line_text">
+            <a>Заказать звонок</a>
+        </div>
+    </div>
+</template>
+
+<script>
+</script>
+
+<style lang="scss">
+    .call{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        .phone_number{
+            display: flex;
+            flex-direction: row;
+            align-items: center;
+            gap: 6px;
+            margin: 0;
+            max-height: 28px;
+        }
+        .second_line_text{
+            align-self: flex-end;
+            
+        }
+    }
+</style>
--- src/components/layout/AppHeader.vue
+++ src/components/layout/AppHeader.vue
@@ -1,18 +1,24 @@
 <script setup lang="ts">
 import Logo from '@/components/ui/Logo.vue';
 import Emblem from '@/components/header/emblem.vue';
-import user from '@/components/header/user.vue'
+import user from '@/components/header/user.vue';
+import call from '@/components/header/call.vue';
 
 </script>
 
 <template>
   <div class="header">
-    <Logo />
-    <Emblem />
-    <div>
-      <span>Производитель турецкого трикотажного полотна</span>
+    <div class="container">
+      <Logo />
+      <Emblem />
+      <div>
+        <span>Производитель турецкого трикотажного полотна</span>
+      </div>
     </div>
-    <user />
+    <div class="container">
+      <user />
+      <call />
+    </div>
   </div>
 </template>
 
@@ -21,9 +27,17 @@ import user from '@/components/header/user.vue'
   display: flex;
   flex-direction: row;
   width: 100%;
-  padding: 40px 80px;
+  padding: 40px 0px;
   align-items: center;
   background-color: var(--main-color);
   color: #fff;
+  justify-content: space-between;
+  .container{
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    padding: 0px 80px;
+    gap: 8px
+  }
 }
 </style>
