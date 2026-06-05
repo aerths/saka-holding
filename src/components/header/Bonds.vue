22bb7ac0a1d189bfc349d4ef64e8311198608633
ZirulanPGS <ilinsergij4298@gmail.com>
2026-06-05 12:09:00 +0300

    add: bounds button

--- src/components/header/Bonds.vue
+++ src/components/header/Bonds.vue
@@ -1,41 +1,65 @@
 <template>
     <div class="bonds">
+        
+        <button class="BB">
+            <a>АКЦИИ</a>
+        </button> 
+        
         <div class="container">
-            <img src="../../assets/icons/bonds/Biege-ellipse.png" width="24px" height="24px">
+            <img src="../../assets/icons/bonds/Purple-ellipse.png" width="24px" height="24px">
             <h4>Кулинарная гладь</h4>
         </div>
+
+        
+
+        <div class="container">
+            <img src="../../assets/icons/bonds/Grey-ellipse.png" width="24px" height="24px">
+            <h4>Футер</h4>
+        </div>
+
         <div class="container">
             <img src="../../assets/icons/bonds/Brown-ellipse.png" width="24px" height="24px">
             <h4>Кулинарная гладь</h4>
         </div>
         <div class="container">
-            <img src="../../assets/icons/bonds/Dark-blue-ellipse.png" width="24px" height="24px">
-            <h4>Кулинарная гладь</h4>
-        </div>
-        <div class="container">
-            <img src="../../assets/icons/bonds/Dark-purple-ellipse.png" width="24px" height="24px">
-            <h4>Кулинарная гладь</h4>
-        </div>
-        <div class="container">
-            <img src="../../assets/icons/bonds/Grey-ellipse.png" width="24px" height="24px">
-            <h4>Кулинарная гладь</h4>
-        </div>
-        <div class="container">
-            <img src="../../assets/icons/bonds/Light-green-ellipse.png" width="24px" height="24px">
-            <h4>Кулинарная гладь</h4>
-        </div>
-        <div class="container">
             <img src="../../assets/icons/bonds/Orange-ellipse.png" width="24px" height="24px">
-            <h4>Кулинарная гладь</h4>
+            <h4>Пике</h4>
         </div>
+
         <div class="container">
             <img src="../../assets/icons/bonds/Pink-ellipse.png" width="24px" height="24px">
-            <h4>Кулинарная гладь</h4>
+            <h4>Интерлок</h4>
         </div>
+
         <div class="container">
-            <img src="../../assets/icons/bonds/Purple-ellipse.png" width="24px" height="24px">
-            <h4>Кулинарная гладь</h4>
+            <img src="../../assets/icons/bonds/Light-green-ellipse.png" width="24px" height="24px">
+            <h4>Капитоний</h4>
         </div>
+
+        <div class="container">
+            <img src="../../assets/icons/bonds/Dark-blue-ellipse.png" width="24px" height="24px">
+            <h4>Велюр</h4>
+        </div>
+
+        <div class="container">
+            <img src="../../assets/icons/bonds/Dark-purple-ellipse.png" width="24px" height="24px">
+            <h4>Селаник</h4>
+        </div>
+
+        <div class="container">
+            <img src="../../assets/icons/bonds/Biege-ellipse.png" width="24px" height="24px">
+            <h4>Вискоза</h4>
+        </div>
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
         
     </div>
 </template>
@@ -47,16 +71,32 @@
     .bonds{
         display: flex;
         flex-direction: row;
-        width: 100%;
+        max-width: 100%;
         background-color: var(--main-color);
-        padding: 25px 85px;
+        padding: 0px 85px;
         justify-content: space-between;
+        height: 50px;
         .container{
-            max-height: 50px;
+            max-height: 100%;
             gap: 6px;
-            max-width:150px
+            max-width:150px;
+        }
+
+    
+        
+    .BB{
+            border: none;
+            padding: 12px 18px;
+            background-color: var(--accent-color);
+            
+            clip-path: polygon(0 0, 100% 0, 100% calc(100% - 50px), calc(100% - 50px) 100%, 0 100%);
+            width: 100%;
+            height: 100%;
+            max-width: 140px;
     }
+        
     }
+    
     
     h4{
         color: #FFF;
