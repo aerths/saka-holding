221a641a3b269d8c5c967a42e605009709c48d88
unknown <@ngknn.local>
2026-06-08 14:29:20 +0300

    add:component tshort

---
+++ src/components/clothes/tshort.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+defineProps<{
+    text: string
+    image: string
+}>()
+</script>
+
+<template>
+    <div class="block_clothes">
+        <div class="img_clothes">
+            <img :src="image" alt="">
+        </div>
+        <div class="text_clothes">
+            <p>
+                {{text}}
+            </p>
+        </div>
+    </div>
+</template>
+
+<style setup lang="scss">
+    .block_clothes {
+        display: flex;
+        flex-direction: column;
+    }
+</style>
