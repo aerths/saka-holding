7db10214823fec4416a124bb2fbcdd4808caa43d
unknown <@ngknn.local>
2026-06-10 11:26:14 +0300

    add:pages

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,11 +2,16 @@ import {
     createRouter,
     createWebHistory
 } from "vue-router"
+
 import HomePage from "../pages/HomePage.vue"
+import CatalogPage from "../pages/CatalogPage.vue"
+import CompanyPage from "../pages/CompanyPage.vue"
 
 export const router = createRouter({
     history: createWebHistory(),
     routes: [
         { path: '/', component: HomePage},
+        { path: '/about', component: CompanyPage},
+        { path: '/catalog', component: CatalogPage},
     ]
 })
