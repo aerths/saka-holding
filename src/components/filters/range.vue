afa9892349f7754403a62e738bef3a01bb8bf036
ZirulanPGS <ilinsergej4298@gmail.com>
2026-06-11 10:30:29 +0300

    change: input range

--- src/components/filters/range.vue
+++ src/components/filters/range.vue
@@ -2,12 +2,11 @@
 </script>
 <template>
     <div class="range">
-        <div class="value">2000₽</div>
+        <input type="range" min="0" max="100000" value="2000">
         <div class="slider-line">
         <span>0₽</span>
         <span>100 000₽</span>
         </div>
-        <input type="range" min="0" max="100000" value="2000">
     </div>    
     
 </template>
@@ -18,21 +17,39 @@
         width: 100%;
         max-width: 260px;
         height: 70px;
-        padding-top:16px;
-        .value{
-            text-align: center;
 
-        }
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        padding: 0 20px;
+        gap: 7px;
         .slider-line{
             display: flex;
             justify-content: space-between;
+            span{
+                font-size: 10px;
+                font-weight: Medium;
+                color: rgba(0, 0, 0, 0.493);
+            }
         }
         input{
             appearance: none;
             outline: none;
             border-radius: 50px;
             background: var(--accent-color);
+            height: 3px;
+            caret-color: var(--accent-color);
         }
+        input[type="range"]::-webkit-slider-thumb {
+            -webkit-appearance: none;
+            appearance: none;
+            width: 14px;
+            height: 14px;
+            background: var(--accent-color); 
+            border-radius: 50%;
+            cursor: pointer;
+}
+
         
     
     }
